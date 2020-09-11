8baaee3176a880b208da7e398d0391496d21a203
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2020-09-11 17:18:07 -0400

    rendering first handsontable

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HotTable } from '@handsontable/react';
+import Handsontable from 'handsontable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handsontableData = Handsontable.helper.createSpreadsheetData(6, 10);
+  }
+
+  render() {
+    return (
+      <div>
+        <HotTable
+          id="hot"
+          data={this.handsontableData}
+          colHeaders={true}
+          rowHeaders={true}
+          />
+      </div>
+    );
+  }
 }
-
-export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import Handsontable from "handsontable";
+import { HotTable, HotColumn } from "@handsontable/react";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+import "handsontable/dist/handsontable.min.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hotSettings: {
+        data: [
+          ["Obrien Fischer"],
+          ["Alexandria Gordon"],
+          ["John Stafford"],
+          ["Regina Waters"],
+          ["Kay Bentley"],
+          ["Emerson Drake"],
+          ["Dean Stapleton"]
+        ],
+        rowHeaders: true,
+        licenseKey: "non-commercial-and-evaluation"
+      }
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <HotTable settings={this.state.hotSettings}>
+        </HotTable>
+      </>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
