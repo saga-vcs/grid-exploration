dc443d8fb6af9c086fae1c5f247211da56d9157c
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2020-09-11 20:09:19 -0400

    added timing metrics

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { HotTable } from '@handsontable/react';
-import Handsontable from 'handsontable';
+import logo from './logo.svg';
+import './App.css';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handsontableData = Handsontable.helper.createSpreadsheetData(6, 10);
-  }
-
-  render() {
-    return (
-      <div>
-        <HotTable
-          id="hot"
-          data={this.handsontableData}
-          colHeaders={true}
-          rowHeaders={true}
-          />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,37 +1,69 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Handsontable from "handsontable";
-import { HotTable, HotColumn } from "@handsontable/react";
+import { HotTable } from "@handsontable/react";
 import * as serviceWorker from './serviceWorker';
 
+import "handsontable/dist/handsontable.min.css"; 
 
-import "handsontable/dist/handsontable.min.css";
+async function generateData() {
+  console.log("starting to generate data")
+  var startGeneratingData = performance.now()
+
+  var i = 0;
+  var tableData = [];
+  for (i; i<1000000; i++) {
+    tableData.push([i,i,i,i,i,i,i,i,i,i,i,i])
+  }
+
+  var finishGenerateDataTime = performance.now()
+
+  console.log("began generating the data ", startGeneratingData)
+  console.log("fished generating data ", finishGenerateDataTime)
+  return tableData
+}; 
 
 class App extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       hotSettings: {
-        data: [
-          ["Obrien Fischer"],
-          ["Alexandria Gordon"],
-          ["John Stafford"],
-          ["Regina Waters"],
-          ["Kay Bentley"],
-          ["Emerson Drake"],
-          ["Dean Stapleton"]
-        ],
+        data: [],
         rowHeaders: true,
         licenseKey: "non-commercial-and-evaluation"
       }
     };
+
+    this.loadData = this.loadData.bind(this);
+
+  }
+
+  async loadData()  {
+    console.log("loading the data")
+    const tableData = await generateData();
+    this.setState({hotSettings: {
+      data: tableData,
+      rowHeaders: true,
+      licenseKey: "non-commercial-and-evaluation"
+    }});
+    console.log("state set")
+
+    var finishedDisplayingTable = performance.now()
+    console.log("finished displaying table", finishedDisplayingTable)
+
+
+
   }
 
   render() {
+    const tableSettings = this.state.hotSettings;
+
     return (
       <>
-        <HotTable settings={this.state.hotSettings}>
-        </HotTable>
+        <div>
+          <button onClick={(e) => this.loadData()}>load data</button>
+        </div>
+        <HotTable settings={tableSettings}></HotTable>
       </>
     );
   }
@@ -44,4 +76,5 @@ ReactDOM.render(<App />, rootElement);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
